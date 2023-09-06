7341c0806e2f9d2d9a36073bb909ef79ebff88fd
Horacio Mota <horaciomota93@gmail.com>
2023-09-06 19:45:54 +0100

    Initial Commit

---
+++ RickAndMortyApp/RickAndMortyAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  RickAndMortyAppApp.swift
+//  RickAndMortyApp
+//
+//  Created by Horacio Mota on 06/09/2023.
+//
+
+import SwiftUI
+
+@main
+struct RickAndMortyAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

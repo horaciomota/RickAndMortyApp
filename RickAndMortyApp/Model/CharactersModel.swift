7afbf868d1a8da41e37f77aa442d1c729a933c61
Horacio Mota <horaciomota93@gmail.com>
2023-09-06 19:52:00 +0100

    CharacterModel and Networking level added to the project

---
+++ RickAndMortyApp/Model/CharactersModel.swift
@@ -0,0 +1,17 @@
+//
+//  CharactersModel.swift
+//  RickAndMortyApp
+//
+//  Created by Horacio Mota on 06/09/2023.
+//
+
+import Foundation
+
+struct CharactersModel: Codable {
+    var id: Int
+    var name: String
+    var gender: String
+    var image: String
+}
+
+
---
+++ RickAndMortyApp/Networking/charactersApiCall.swift
@@ -0,0 +1,14 @@
+//
+//  charactersApiCall.swift
+//  RickAndMortyApp
+//
+//  Created by Horacio Mota on 06/09/2023.
+//
+
+import Foundation
+
+func charactersApiCall() async {
+    
+    
+    
+}

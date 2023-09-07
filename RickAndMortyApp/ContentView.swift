a566fca35d8949f380c5f5148d8d345965d6d3ac
Horacio Mota <horaciomota93@gmail.com>
2023-09-07 18:02:08 +0100

    App renamed

--- RickAndMortyApp/ContentView.swift
+++ RickAndMortyApp/ContentView.swift
@@ -2,43 +2,13 @@ import SwiftUI
 
 struct ContentView: View {
     @State private var characters: [CharactersModel] = []
-    @State private var listOfCoins: [CoinData] = []
-
 
     var body: some View {
-        NavigationView {
-            List(listOfCoins, id: \.self) { coin in
-                Text(coin.name)
-                Text(coin.rank)
-
-            }
-            .onAppear {
-                Task {
-                    do {
-                        listOfCoins = try await getDataCoins()
-                    } catch {
-                        print("Erro ao obter dados: \(error)")
-                    }
-                }
-            }
-
-            .navigationBarTitle("Character List")
+        VStack {
+            Text("Testando")
         }
     }
 
-
-    func fetchCharacters() async throws -> [CharactersModel] {
-            guard let url = URL(string: "https://rickandmortyapi.com/api/character") else {
-                print("Api conectada com sucesso")
-                throw URLError(.badURL)
-
-            }
-
-            let (data, _) = try await URLSession.shared.data(from: url)
-            let dataResponse = try JSONDecoder().decode(CharactersResponse.self, from: data)
-            print("Busca de personagens bem-sucedida")
-            return dataResponse.results
-        }
     }
 
 

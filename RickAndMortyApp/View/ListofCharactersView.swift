9b9e2668133026b53919e90a10df34acee72d0ee
Horacio Mota <horaciomota93@gmail.com>
2023-09-07 17:45:50 +0100

    List of characters added

---
+++ RickAndMortyApp/View/ListofCharactersView.swift
@@ -0,0 +1,20 @@
+//
+//  ListofCharactersView.swift
+//  RickAndMortyApp
+//
+//  Created by Horacio Mota on 07/09/2023.
+//
+
+import SwiftUI
+
+struct ListofCharactersView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct ListofCharactersView_Previews: PreviewProvider {
+    static var previews: some View {
+        ListofCharactersView()
+    }
+}

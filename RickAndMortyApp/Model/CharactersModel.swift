7fe302114765c4662de4bbdeccc503ef79173fee
Horacio Mota <horaciomota93@gmail.com>
2023-09-07 17:53:33 +0100

    Function fetchCharacter working! Api connection perfect

--- RickAndMortyApp/Model/CharactersModel.swift
+++ RickAndMortyApp/Model/CharactersModel.swift
@@ -17,28 +17,5 @@ struct CharactersModel: Codable {
     var gender: String
 }
 
-struct jsonCoinData: Codable, Hashable {
-    var data: [CoinData]
-}
-
-struct CoinData: Codable, Hashable {
-    var id: String
-    var name: String
-    var rank: String
-}
-
-func getDataCoins() async throws -> [CoinData] {
-    
-    guard let url = URL(string: "https://api.coincap.io/v2/assets")
-    else {
-        print("Something Wrong with Your URL")
-        throw URLError(.badURL)
-    }
-    let (data, _) = try await URLSession.shared.data(from: url)
-    let coinsResponse = try JSONDecoder().decode(jsonCoinData.self, from: data)
-    print("Busca na API concluida")
-    return coinsResponse.data
-}
-
 
 
--- RickAndMortyApp/Networking/charactersApiCall.swift
+++ RickAndMortyApp/Networking/charactersApiCall.swift
@@ -1,1 +1,17 @@
 
+
+import SwiftUI
+
+
+func fetchCharacter() async throws -> [CharactersModel] {
+    
+    guard let url = URL(string: "https://rickandmortyapi.com/api/character") else {
+        print("Something wrong with URL")
+        throw URLError(.badURL)
+    }
+    
+    let (data, _) = try await URLSession.shared.data(from: url)
+    let characterDataResponse = try JSONDecoder().decode(CharactersResponse.self, from: data)
+    return characterDataResponse.results
+    
+}
